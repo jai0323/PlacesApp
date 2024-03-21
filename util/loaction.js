ffd6d053bcd5ccb8c4bbc74601399e0c8b29ded8
jai0323 <jaishobhit45@gmail.com>
2024-03-21 23:16:55 +0530

    Navigation and screen

--- util/loaction.js
+++ util/loaction.js
@@ -1,11 +1,11 @@
 const GOOGLE_API_KEY = '';
 
 export function getMapPreview(lat,lng){
-    const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap
-    &markers=color:red%7Clabel:S%7C${lat},${lng}
-    &key=${GOOGLE_API_KEY}`;
+    // const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap
+    // &markers=color:red%7Clabel:S%7C${lat},${lng}
+    // &key=${GOOGLE_API_KEY}`;
 
-    const imagePreviewUrl = 'https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key="GOOGLE API KEY'
+    // imagePreviewUrl = 'https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key="GOOGLE API KEY'
 
     return imagePreviewUrl;
 }
